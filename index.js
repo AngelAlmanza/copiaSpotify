bd7cea5c23369006a328c2ba42093728f41db5b8
Angel Almanza <almanza.angel245@gmail.com>
2023-07-26 16:41:19 -0700

    Animacion del menu de hamburguesa

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const menuHamburger = document.querySelector('#menu-hamburger');
 const linesMenu = document.querySelectorAll('#menu-hamburger > span');
 const main = document.querySelector('main');
 const footer = document.querySelector('footer');
-console.log(linesMenu);
+const menu = document.querySelector('.menu-nav');
+const menuContainer = document.querySelector('#menu-container');
+const logo = document.querySelector('.logo');
 
 menuHamburger.addEventListener('click', () => {
+
   linesMenu.forEach(element => {
     element.classList.toggle('open');
   });
-  setTimeout(() => {
-    main.classList.toggle('inactive');
-  }, 100);
-  setTimeout(() => {
-    footer.classList.toggle('inactive');
-  }, 100);
+
+  main.classList.toggle('inactive');
+  footer.classList.toggle('inactive');
+  menuContainer.classList.toggle('inactive')
+  menuContainer.classList.toggle('menu-container')
+  menu.classList.toggle('inactive');
+  logo.classList.toggle('inactive-opacity')
+
 });
